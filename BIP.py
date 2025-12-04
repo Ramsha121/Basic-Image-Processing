61d1cb8a573f40cc60e86a4b99179964d3527666
Data Scientist <89708093+Ramsha121@users.noreply.github.com>
2025-12-04 13:36:37 +0530

    Update BIP.py

--- BIP.py
+++ BIP.py
@@ -29,7 +29,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-# -------------------- SIDE MENU --------------------
+# -------------------- SIDE MENU WITH FIXED LABELS --------------------
 st.sidebar.title("📌 Image Processing Menu")
 
 menu_options = {
